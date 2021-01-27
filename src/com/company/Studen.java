084da9c890e8183f0130f367fc21e7f0825b3d8e
truongquangdang97 <70947896+truongquangdang97@users.noreply.github.com>
2021-01-27 11:13:43 +0700

    test

---
+++ src/com/company/Studen.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Studen {
+    int id ;
+    String name ;
+    int age ;
+
+    void run(){
+        System.out.println(this.name+"Running ...");
+    }
+    void est(){
+        System.out.println(this.name+"eating...");
+    }
+}
